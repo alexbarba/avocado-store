85fe07237f93e3e15b718286415dc6b84596f3ac
Alexis Barba <alexbarba.2278@gmail.com>
2020-09-14 22:46:13 -0500

    Empezando con UI. Estamos en la parte de getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+export const getServerSideProps = async () => {
+  const response = await fetch('https://avo-store.alexbarba.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+  return {
+    props: {
+      productList,
+    },
+  }
+}
 
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
